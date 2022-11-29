00673b6b030af0c7e6cc92d996ee0aa83d54e102
sudaksudak <sveta_kiev_ua@yahoo.com>
2022-11-29 10:17:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const options = [ "rock", "scissors", "paper" ];
+let myGame = prompt("Do you want to play a game?");
+if(myGame =="Yes"){
+        alert("Lets's GO");
+}else{
+        alert("Bye!");
+}
+
